9fa290812cdc0ca30d9aa79a328cde4788dde282
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2025-11-27 13:41:42 +0000

    feat: adicionando titulo personalizado para a pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Helmet } from "react-helmet";
 import Router from "./routers/Router";
 import "./styles/global.css"
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Título da Página</title>
+        <meta name="description" content="Minha descrição personalizada." />
+      </Helmet>
       <Router />
     </div>
   );
--- src/pages/NotFound.page.tsx
+++ src/pages/NotFound.page.tsx
@@ -1,13 +1,20 @@
+import { Helmet } from "react-helmet"
 import css from "../styles/pages/notFound.module.css"
 
 function NotFoundPage() {
     return (
-        <main className={css.main}>
-            <div>
-                <h1 className={css.title}>404 - Not Found</h1>
-                <p><a href="/">Vá para a página inicial.</a></p>
-            </div>
-        </main>
+        <>
+            <Helmet>
+                <title>Página não encontrada.</title>
+                <meta name="description" content="A página não existe ou você não possui acesso." />
+            </Helmet>
+            <main className={css.main}>
+                <div>
+                    <h1 className={css.title}>404 - Not Found</h1>
+                    <p><a href="/">Vá para a página inicial.</a></p>
+                </div>
+            </main>
+        </>
     )
 }
 
